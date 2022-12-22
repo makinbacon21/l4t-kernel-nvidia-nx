3950687d18f689faf72019585167de61f263c358
CTCaer <ctcaer@gmail.com>
2022-12-22 15:56:56 +0200

    tegra210: clk: fixup DVFS for T210B01
    - Fix various bugs, missing frequencies and missing entries for T210B01
    - Add OC entries
    - Add support for max 2133MHz RAM clock.

--- drivers/soc/tegra/tegra210-dvfs.c
+++ drivers/soc/tegra/tegra210-dvfs.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
  * Copyright (c) 2014-2022, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2021-2023, CTCaer
  */
 
 #include <linux/kernel.h>
@@ -551,7 +552,11 @@ static struct cpu_dvfs cpu_fv_dvfs_table[] = {
 		{ 1785000000UL, {  1120000,        0,        0 } }, \
 		{ 1887000000UL, {  1120000,        0,        0 } }, \
 		{ 1963500000UL, {  1120000,        0,        0 } }, \
+		{ 2065500000UL, {  1120000,        0,        0 } }, \
 		{ 2091000000UL, {  1120000,        0,        0 } }, \
+		{ 2193000000UL, {  1235000,        0,        0 } }, \
+		{ 2295000000UL, {  1235000,        0,        0 } }, \
+		{ 2397000000UL, {  1235000,        0,        0 } }, \
 		{ 0,	        { } }, \
 	}, \
 	.pll_min_millivolts = 800
@@ -580,6 +585,10 @@ static struct cpu_dvfs cpu_fv_dvfs_table[] = {
 		{ 1887000000UL, {  1120000,        0,        0 } }, \
 		{ 1963500000UL, {  1120000,        0,        0 } }, \
 		{ 2014500000UL, {  1120000,        0,        0 } }, \
+		{ 2091000000UL, {  1235000,        0,        0 } }, \
+		{ 2193000000UL, {  1235000,        0,        0 } }, \
+		{ 2295000000UL, {  1235000,        0,        0 } }, \
+		{ 2397000000UL, {  1235000,        0,        0 } }, \
 		{ 0,	        { } }, \
 	}, \
 	.pll_min_millivolts = 800
@@ -588,13 +597,13 @@ static struct cpu_dvfs cpub01_fv_dvfs_table[] = {
 	{
 		.speedo_id = 2,
 		.process_id = -1,
-		.max_mv = 1120,
+		.max_mv = 1235, /* Allow OC max voltage of 1235 mV from 1120 */
 		CPUB01_PLL_CVB_TABLE_SLT,
 	},
 	{
 		.speedo_id = -1,
 		.process_id = -1,
-		.max_mv = 1120,
+		.max_mv = 1235, /* Allow OC max voltage of 1235 mV from 1120 */
 		CPUB01_PLL_CVB_TABLE,
 	},
 };
