4e50a36e7e686242335b1ba6a7c6003b975fbc5b
Thomas Makin <halorocker89@gmail.com>
2024-05-26 21:50:43 +0000

    Revert "soc: tegra: dvfs: support multi railed dvfs"
    This reverts commit c01096dc1167336fc38c05cf3afa4c2367672c29.

--- drivers/soc/tegra/tegra-dvfs.c
+++ drivers/soc/tegra/tegra-dvfs.c
@@ -502,7 +502,7 @@ static unsigned long *dvfs_get_freqs(struct dvfs *d)
 static int __tegra_dvfs_set_rate(struct dvfs *d, unsigned long rate)
 {
 	int i = 0;
-	int ret;
+	int ret, mv;
 	unsigned long *freqs = dvfs_get_freqs(d);
 	const int *millivolts = dvfs_get_millivolts(d, rate);
 
@@ -535,6 +535,7 @@ static int __tegra_dvfs_set_rate(struct dvfs *d, unsigned long rate)
 			return -EINVAL;
 		}
 
+		mv = millivolts[i];
 		d->cur_millivolts = millivolts[i];
 	}
 
@@ -557,23 +558,6 @@ static struct dvfs *tegra_clk_to_dvfs(struct clk *c)
 		list_for_each_entry(d, &rail->dvfs, reg_node) {
 			if (clk_is_match(c, d->clk))
 				return d;
-		}
-	}
-	return NULL;
-}
-
-static struct dvfs *tegra_clk_to_multirailed_dvfs(struct clk_notifier_data *cnd)
-{
-	struct dvfs *d;
-	struct dvfs_rail *rail;
-
-	list_for_each_entry(rail, &dvfs_rail_list, node) {
-		list_for_each_entry(d, &rail->dvfs, reg_node) {
-			if (clk_is_match(cnd->clk, d->clk)) {
-				if (d->multi_rail && cnd->new_rate == d->cur_rate)
-					break;
-				return d;
-			}
 		}
 	}
 	return NULL;
@@ -1033,7 +1017,7 @@ static int tegra_dvfs_clk_event(struct notifier_block *this,
 	struct dvfs *d;
 	int new_mv, err = 0;
 
-	d = tegra_clk_to_multirailed_dvfs(cnd);
+	d = tegra_clk_to_dvfs(cnd->clk);
 	if (d == NULL)
 		return NOTIFY_DONE;
 
