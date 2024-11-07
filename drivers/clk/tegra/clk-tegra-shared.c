b854a1690f599512446bbf78e53fbdd5fc1c9b63
Thomas Makin <halorocker89@gmail.com>
2024-11-07 23:45:54 +0000

    ditch bwmgr emc clock
    Change-Id: I1cafb4b5771e7973a5565d707314899453a61601

--- drivers/clk/tegra/clk-tegra-shared.c
+++ drivers/clk/tegra/clk-tegra-shared.c
@@ -114,7 +114,6 @@ static struct tegra_shared_clk shared_clks[] = {
 	SHARED_CLK("sdmmc4.sclk", "ahb.sclk", 0, 0, NULL, tegra_clk_sdmmc4_ahb_sclk),
 	SHARED_LIMIT("vic.floor.cbus", "c2bus", 0, 0, NULL, tegra_clk_vic_floor_cbus),
 	SHARED_CLK("override.emc", "emc_master", SHARED_OVERRIDE, 0, NULL, tegra_clk_override_emc),
-	SHARED_CLK("bwmgr.emc", "emc_master", 0, 0, NULL, tegra_clk_bwmgr_emc),
 	SHARED_CLK("vcm.sclk", "sbus", 0, 0, NULL, tegra_clk_vcm_sclk),
 	SHARED_CLK("vcm.ahb.sclk", "ahb.sclk", 0, 0, NULL, tegra_clk_vcm_ahb_sclk),
 	SHARED_CLK("vcm.apb.sclk", "apb.sclk", 0, 0, NULL, tegra_clk_vcm_apb_sclk),
